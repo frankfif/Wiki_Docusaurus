4fb171de5f981ab39e3adc985d29b26326da0e57
frankfif <50836996+frankfif@users.noreply.github.com>
2022-12-03 19:27:22 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -114,7 +114,7 @@ const config = {
       algolia: {
 
         apiKey: '02bd2340879bdb682f2a9fe509fec240',//5c07d8bf9c9928c4453857f6cad0420e?
-        indexName: 'wiki-power', //
+        indexName: 'frankfif', //
 
         // The application ID provided by Algolia
         appId: 'IRO903CONI', //BH4D9OD16A 是默认的，文档见 https://autocomplete-experimental.netlify.app/docs/docsearchmodal/#appid，用自己的 ID IRO903CONI 反而搜不出内容 
